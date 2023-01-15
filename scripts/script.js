85fa799746343067545373a43b1b700b4e9ca351
cthorne7 <chelseelee07@gmail.com>
2023-01-14 21:17:29 -0700

    date

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
+let yearDate = new Date().getFullYear();
+let footer = document.querySelector("#date");
+footer.innerHTML += yearDate;
+
 document.querySelector(
 	"#lastModified"
 ).textContent = `Last Modification: ${document.lastModified}`;
 
 
-let yearDate = new Date().getFullYear();
-let footer = document.querySelector("footer");
-footer.innerHTML += yearDate;
